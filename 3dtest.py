172fc82449f4bfa4955dec4e1def80938132e26d
usubazzz <usubazzz@gmail.com>
2021-12-09 00:47:16 +0900

    ConvexMeshクラスの追加
    いくつかの関数をクラスに以降した

--- 3dtest.py
+++ 3dtest.py
@@ -5,10 +5,109 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import sympy
 
+import copy
+
+
 
 ###     # 法線ベクトルは原点を中心としてる
 ### 法線ベクトルの向きは，重心よりも外を向くようにする
 ### そのため，simplexの重心を知る必要がある
+
+class ConvexMesh():
+    def __init__(self, _points, _dim):
+        self.dim = _dim
+        self.points = _points
+        self.simplex = None
+        self.facets = [self.generate_first_facet()]
+
+        self.centroid = self.calc_centroid()
+
+        self.asiign_facets_to_points()
+
+
+    def calc_centroid(self):
+        _simplex_points_id = []
+        _centroid = np.array([0.0 for i in range(self.dim)])
+        for f in self.facets:
+            _simplex_points_id.extend(f.points_id)
+
+        _uniq_id = list(set(_simplex_points_id))
+
+        for _id in _uniq_id:
+            _centroid += np.array(self.points[_id])
+        _centroid /= len(_uniq_id)
+
+        return _centroid
+
+    def generate_first_facet(self):
+        _maxs = [0 for i in range(self.dim)]
+        _ids  = [0 for i in range(self.dim)]
+
+        for i in range(len(self.points)):
+            for j in range(len(self.points[0])):
+                p_abs = np.abs(self.points[i][j])
+                if p_abs > _maxs[j]:
+                    _maxs[j] = p_abs
+                    _ids[j] = i
+
+        # 各次元の最大値となる点をシンプレックスの点とする
+        return(Facet(_ids, self.points))
+
+    def asiign_facets_to_points(self):
+        for i, p in enumerate(self.points):
+            for j, f in enumerate(self.facets):
+                if i in f.points_id:
+                    continue                    # ファセットを構成する点は無視
+                v = np.array(p) - np.array(f.centroid)
+                dot = np.dot(f.normal, v)
+                if dot >= 0:
+                    print("p{} -> f{}".format(i, j))
+                    f.out_points_id.append(i)   # p を fに割り当てる
+                    break
+
+    # 最遠の点を取り出す
+    def pick_farthest_point(self, facet, _points):
+        if facet.out_points_id == []:
+            print("set out points")
+            return None
+
+        _max_d = 0
+        _max_id = 0
+
+        dO = np.dot(facet.normal, _points[facet.points_id[0]])    # 原点から超平面までの距離
+        for id in facet.out_points_id:
+            dOP = np.dot(facet.normal, _points[id])           # 超平面の法線に沿った原点から点Pまでの距離
+            dP = np.abs(dOP - dO)                               # 超平面から点Pまでの距離
+            if dP > _max_d:
+                _max_d = dP
+                _max_id = id
+
+        print("d_max id: {}".format(_max_id))
+
+        return _max_id
+
+    # 最遠の点が超平面の上になるファセットを取り出す
+    def pick_visible_facet(self, point):
+        _visible_facets = []
+        for i, f in enumerate(self.facets):
+            v = np.array(point) - np.array(self.centroid)
+            dot = np.dot(f.normal, v)
+            # print("v: {}, n: {}, nv: {}".format(point, f.normal, dot))
+            if dot > 0:
+                print("visible: facet {}".format(i))
+                _visible_facets.append(f)
+
+        return _visible_facets
+
+    # 各ファセットで共通しない点を取得(隣接するファセットの端の点)
+    def get_unique_points(self, facets):
+        _all_points_id = []
+
+        for f in facets:
+            _all_points_id.extend(f.points_id)
+
+        return [x for x in set(_all_points_id) if _all_points_id.count(x) == 1]
+
 
 class Point():
     def __init__(self):
@@ -63,6 +162,10 @@ class Facet():
         return edge
 
     def calc_centroid(self):
+        # _centroid = np.array([0 for i in range(self.dim)])
+
+
+
         A = [self.points[i] for i in sorted(self.points_id)]
         c = [0.0 for i in range(self.dim)]
 
@@ -78,49 +181,9 @@ class Facet():
         return c
 
 
-def pick_farthest_point(facet, _points):
-    if facet.out_points_id == []:
-        print("set out points")
-        return None
 
-    _max_d = 0
-    _max_id = 0
 
-    dO = np.dot(facet.normal, _points[facet.points_id[0]])    # 原点から超平面までの距離
-    for id in facet.out_points_id:
-        dOP = np.dot(facet.normal, _points[id])           # 超平面の法線に沿った原点から点Pまでの距離
-        dP = np.abs(dOP - dO)                               # 超平面から点Pまでの距離
-        if dP > _max_d:
-            _max_d = dP
-            _max_id = id
 
-    print("d_max id: {}".format(_max_id))
-
-    return _max_id
-
-# 最遠の点が超平面の上になるファセットを取り出す
-def pick_visible_facet(facets, point):
-
-    
-    _visible_facets = []
-    for i, f in enumerate(facets):
-        v = np.array(point) - np.array(f.centroid)
-        dot = np.dot(f.normal, v)
-        # print("v: {}, n: {}, nv: {}".format(point, f.normal, dot))
-        if dot > 0:
-            print("visible: facet {}".format(i))
-            _visible_facets.append(f)
-
-    return _visible_facets
-
-# 各ファセットで共通しない点を取得(隣接するファセットの端の点)
-def get_unique_points(facets):
-    _all_points_id = []
-
-    for f in facets:
-        _all_points_id.extend(f.points_id)
-
-    return [x for x in set(_all_points_id) if _all_points_id.count(x) == 1]
 
 
 
@@ -145,62 +208,50 @@ print("seed: {}".format(seed))
 
 points = [[(rnd_max-rnd_min)*rd.random() + rnd_min for i in range(D)] for j in range(N)]
 
-# points = [[1, 1], [3, -1], [2, 4], [4, 5], [-3, -5]]
-# points = [[1, 1, 2], [0, -2, 1], [3, -1, 0]]
-# points = [[1, 1, 2, 3], [0, -2, 1, 5], [3, -1, 0, 9], [0, 0, 1, 2], [2, 3, 4, 2]]
 
-face1 = Facet([0, 1], points)
-face2 = Facet([1, 2], points)
-# face1 = Facet([0, 1, 2], points)
-# face1 = Facet([1, 2, 3, 4], points)
-# face2 = Facet([0, 1, 3], points)
-# face3 = Facet([0, 2, 3], points)
-# face4 = Facet([1, 2, 3], points)
+mesh = ConvexMesh(points, 2)
+
+facets = mesh.facets
+
+facets_stack = copy.deepcopy(facets)
+
+max_id = mesh.pick_farthest_point(facets[0], points)
 
 
-facets = [face1, face2]
+#### ファセットを渡されてもその後の編集ができない，idのほうがまし
+visible_facets = mesh.pick_visible_facet(points[max_id])
+
+if visible_facets != []:
+    uni = mesh.get_unique_points(visible_facets)
+    new_edge = [[max_id, uni[i]] for i in range(len(uni))]
+    print(new_edge)
+
+    for edge in new_edge:
+        new_face = Facet(edge, points)
+        mesh.facets.append(new_face)
+
+    del facets[0]
+
+# in_points = []    # convex の内側にある頂点
 
 
-# 各頂点の割当
-for i, p in enumerate(points):
-    for j, f in enumerate(facets):
-        if i in f.points_id:
-            # ファセットを構成する点は無視
-            continue
-        v = np.array(p) - np.array(f.centroid)
-        dot = np.dot(f.normal, v)
-        if dot >= 0:
-            # p を fに割り当てる
-            print("p{} -> f{}".format(i, j))
-            f.out_points_id.append(i)
-            break
-
-max_id = pick_farthest_point(face1, points)
 
 
-visible_facets = pick_visible_facet(facets, points[max_id])
-
-
-g = get_unique_points(visible_facets)
-print(g)
-
-
-in_points = []    # convex の内側にある頂点
 
 
 
 fig = plt.figure(figsize=plt.figaspect(1.0))
-if face1.dim == 3:
+if mesh.dim == 3:
     ax = Axes3D(fig)
-if face1.dim == 2:
+if mesh.dim == 2:
     ax = fig.add_subplot(1,1,1) # 2D
 
 
 ### 頂点表示
 for v in points:
-    if face1.dim == 3:
+    if mesh.dim == 3:
         ax.scatter(v[0], v[1], v[2], marker="o", c='r', s=60)
-    if face1.dim == 2:
+    if mesh.dim == 2:
         ax.scatter(v[0], v[1], marker="o", c='r', s=20)
 
 edge = []
@@ -209,31 +260,32 @@ for f in facets:
         edge.append(points[e[0]])
         edge.append(points[e[1]])
 
-if face1.dim == 3:
+if mesh.dim == 3:
     line_x = [e[0] for e in edge]
     line_y = [e[1] for e in edge]
     line_z = [e[2] for e in edge]
     ax.plot(line_x, line_y, line_z, "-", color='k')
 
-if face1.dim == 2:
+if mesh.dim == 2:
     line_x = [e[0] for e in edge]
     line_y = [e[1] for e in edge]
     ax.plot(line_x, line_y, "-", color='k')
 
 
 ### 法線表示
-offset = face1.centroid
+offset = mesh.facets[0].centroid
+face = mesh.facets[0]
 
-if face1.dim == 3:
-    ax.quiver(offset[0], offset[1], offset[2], face1.normal[0], face1.normal[1], face1.normal[2])
-if face1.dim == 2:
+if mesh.dim == 3:
+    ax.quiver(offset[0], offset[1], offset[2], face.normal[0], face.normal[1], face.normal[2])
+if mesh.dim == 2:
     for f in facets:
         offset = f.centroid
         ax.plot([offset[0], offset[0] + f.normal[0]], [offset[1], offset[1] + f.normal[1]], "-", color='b')
 
 
 ### 最も遠い頂点
-if face1.dim == 2:
+if mesh.dim == 2:
     ax.scatter(points[max_id][0], points[max_id][1], marker="o", c='b', s=60)
 
 
